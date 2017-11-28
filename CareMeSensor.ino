3c2356290c5dfead6d27cf392460ab54fe2c5526
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2017-11-28 17:33:20 -0500

    fdf
    fd

---
+++ CareMeSensor.ino
@@ -0,0 +1,15 @@
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  sensorValue = map(sensorValue, 0, 700, 0, 100);
+  Serial.println(sensorValue);
+//Serial.write(sensorValue);
+  delay(100);
+}
